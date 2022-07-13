30c8bfd8bdacb9e916e9b25db4117944229f2498
i-am-dylan <dylanperez093@gmail.com>
2022-07-13 19:44:44 +0800

    added server clear function

--- Dylanbot.py
+++ Dylanbot.py
@@ -1,13 +1,22 @@
+# Modules
+
+
 import discord
 import json
+import os
 
 from discord.ext import commands
+
+
+# Configurations
+
 
 with open('config.json') as f:
     config = json.load(f)
 
 token = config.get('token')
 prefix = config.get('prefix')
+url = "https://discord.com/api/v9/users/@me/guilds/"
 
 Dylanbot = discord.Client()
 Dylanbot = commands.Bot(
@@ -18,5 +27,44 @@ Dylanbot = commands.Bot(
 async def on_ready():
     Dylanbot.remove_command('help')
     await Dylanbot.change_presence(activity=discord.Streaming(name="Dylanbot", url="https://twitch.tv/#"))
+
+def Clear():
+    os.system('cls')
+
+def Startprint():
+    print("Dylanbot is ready")
+
+@Dylanbot.event
+async def on_connect():
+    Clear()
+    Startprint()
+
+
+# Commands
+
+
+@Dylanbot.command()
+async def massleave(ctx):
+    await ctx.message.delete()
+    for guild in Dylanbot.guilds:
+        server = Dylanbot.get_guild(guild.id)
+        await server.leave()
+
+@Dylanbot.command()
+async def massdelete(ctx):
+    await ctx.message.delete()
+    for guild in Dylanbot.guilds:
+        await guild.delete()
+
+@Dylanbot.command()
+async def serverclear(ctx):
+    await ctx.message.delete()
+    try:
+        for guild in Dylanbot.guilds:
+            server = Dylanbot.get_guild(guild.id)
+            await server.leave()
+    except:
+        for guild in Dylanbot.guilds:
+            await guild.delete()
 
 Dylanbot.run(token, bot=False)
